01b94bdf5247d7f257c38310675b2e4e9fd7c15c
Mohammed Yussif <mohammed.yussif@invennico.com>
2024-05-08 01:01:43 +0000

    templates

---
+++ cc_intermie_advance/templates_type_alieses_in_cc/Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+// Metaprogramming
+// Creating templates is sometimes referred to as metaprogramming, 
+// which means writing code that writes code.The code we create(a class template)
+// is used to create even more code(classes)
+
+
+// Declaring Template Parameters
+
+template <int nSomeInt>
+class Pair{
+public:
+};
+
+int main() {
+
+	return 0;
+}
