1f4fe3dd72f28efd2f055fed163e0ae18fed6f90
Mohammed Yussif <mohammed.yussif@invennico.com>
2024-05-11 22:45:07 +0000

    Computer Graphics from scratch

--- Computer_graphics/3drender/src/main.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-   printf("Computer Graphics");
-    return 0;
-}
---
+++ Computer_graphics/3drenderer/src/Main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <SDL.h>
+
+int main(int argc, char* argv[]) {
+
+	SDL_Init(SDL_INIT_EVERYTHING);
+	printf("Hello, World!\n");
+	return 0;
+}
