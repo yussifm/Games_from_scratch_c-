ce13f1a71cfe0474358a74119cef13c426cf4e55
Mohammed Yussif <mohammed.yussif@invennico.com>
2024-05-11 21:53:03 +0000

    .

--- Computer_graphics/3drender/src/main.c
+++ Computer_graphics/3drender/src/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    printf("Computer Graphics!\n");
-    printf("Computer Graphics!\n");
+   printf("Computer Graphics");
     return 0;
 }
