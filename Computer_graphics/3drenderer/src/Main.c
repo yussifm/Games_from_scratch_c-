29218523643d4205ba8c93391da4d88b50186106
Mohammed Yussif <mohammed.yussif@invennico.com>
2024-05-16 00:34:46 +0000

    drawing rect and project refactor

--- Computer_graphics/3drenderer/src/Main.c
+++ Computer_graphics/3drenderer/src/Main.c
@@ -2,29 +2,19 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <SDL.h>
+#include "./headers/display.h"
+
 
 // Global Variables
 bool is_running = false;
-SDL_Window* p_window = NULL;
-SDL_Renderer* p_renderer = NULL;
-//
-Uint32* Uint32_color_buffer = NULL;
-SDL_Texture* p_color_buffer_texture = NULL;
-
-//
-int i_Windown_width = 800;
-int i_Windown_height = 600;
 
 // Function Prototypes/Declarations
-bool initialize_window(void); 
 void setup(void);
 void update(void);
 void render(void);
 void process_input(void);
-void destory_window(void);
-void clear_color_buffer(uint32_t color);
-void render_color_buffer(void);
-void draw_grid(void);
+
+
 
 
 // Main Function
@@ -53,53 +43,6 @@ int main(int argc, char* argv[]) {
 
 
 // Function Definitions
-bool initialize_window(void) {
-	// Initialize SDL and eveything including display, mouse, keyboard etc
-	if (SDL_Init(SDL_Init(SDL_INIT_EVERYTHING)) != 0) {
-		fprintf(stderr, "Error initializing SDl. \n");
-		return false;
-	}
-
-	// Use SDL to query what is the fullscreen max
-	
-	SDL_DisplayMode display_mode;
-	SDL_GetCurrentDisplayMode(0, &display_mode);
-
-	// reassigning the width and hight
-	i_Windown_width = display_mode.w;
-	i_Windown_height = display_mode.h;
-
-
-	  // Create a SDL window
-	
-     p_window = SDL_CreateWindow(
-		 NULL, 
-		 SDL_WINDOWPOS_CENTERED,
-		 SDL_WINDOWPOS_CENTERED, 
-		 i_Windown_width, 
-		 i_Windown_height,
-		 SDL_WINDOW_BORDERLESS
-		 );
-	  // if windwo contains content or is a valid pointer
-
-	 if (!p_window) {
-		 fprintf(stderr, "Error Creating SDl Window. \n");
-		 return false;
-	 }
-
-	 // Creating SDl renderer
-	p_renderer = SDL_CreateRenderer(p_window, -1, 0);
-	 if (!p_renderer) {
-		 fprintf(stderr, "Error Creating SDl Renderer. \n");
-		 return false;
-	 }
-
-	 SDL_SetWindowFullscreen(p_window, SDL_WINDOW_FULLSCREEN);
-	 return true;
-
-
-}
-
 
 void process_input(void) {
 	SDL_Event event;
@@ -134,62 +77,18 @@ void setup(void) {
 	);
 }
 //
-void update(void) {}
-//
-void render_color_buffer(void) {
-	SDL_UpdateTexture(
-	p_color_buffer_texture, 
-	NULL, 
-	Uint32_color_buffer, 
-	(int)(i_Windown_width * sizeof(uint32_t))
-
-);
-
-	SDL_RenderCopy(
-      p_renderer,
-      p_color_buffer_texture, 
-      NULL, 
-      NULL
-	);
-
-}
-// 
-// draw grid on Screen
-void draw_grid(void) {
-	for (int y = 0; y < i_Windown_height; y++) {
-		for (int x = 0; x < i_Windown_width; x++) {
-			if (x % 40 == 0 || y % 40 == 0) {
-            Uint32_color_buffer[(i_Windown_width * y) + x] = 0xFF333333;
-		   }
-		}
-	}
-	
-}
-//
-void clear_color_buffer(uint32_t color) {
-	for (int y = 0; y < i_Windown_height; y++) {
-		for (int x = 0; x < i_Windown_width; x++) {
-          Uint32_color_buffer[(i_Windown_width * y) + x] = color;
-		}
-	}
-}
-//
 void render(void) {
 	SDL_SetRenderDrawColor(p_renderer, 255, 0, 0, 255);
-	SDL_RenderClear(p_renderer); 
+	SDL_RenderClear(p_renderer);
 
 	draw_grid();
+	draw_react(200, 300, 100, 400, 0xFF00FF00);
 	render_color_buffer();
 	clear_color_buffer(0xFF000000);
-	
+
 	SDL_RenderPresent(p_renderer);
 }
 
-
+void update(void) {}
 //
-void destory_window(void) {
-	free(Uint32_color_buffer);
-	SDL_DestroyRenderer(p_renderer);
-	SDL_DestroyWindow(p_window);
-	SDL_Quit();
-}
+
---
+++ Computer_graphics/3drenderer/src/display.c
@@ -0,0 +1,121 @@
+#include "./headers/display.h"
+
+
+SDL_Window* p_window = NULL;
+SDL_Renderer* p_renderer = NULL;
+//
+Uint32* Uint32_color_buffer = NULL;
+SDL_Texture* p_color_buffer_texture = NULL;
+
+//
+int i_Windown_width = 800;
+int i_Windown_height = 600;
+
+bool initialize_window(void) {
+	// Initialize SDL and eveything including display, mouse, keyboard etc
+	if (SDL_Init(SDL_Init(SDL_INIT_EVERYTHING)) != 0) {
+		fprintf(stderr, "Error initializing SDl. \n");
+		return false;
+	}
+
+	// Use SDL to query what is the fullscreen max
+
+	SDL_DisplayMode display_mode;
+	SDL_GetCurrentDisplayMode(0, &display_mode);
+
+	// reassigning the width and hight
+	i_Windown_width = display_mode.w;
+	i_Windown_height = display_mode.h;
+
+
+	// Create a SDL window
+
+	p_window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		i_Windown_width,
+		i_Windown_height,
+		SDL_WINDOW_BORDERLESS
+	);
+	// if windwo contains content or is a valid pointer
+
+	if (!p_window) {
+		fprintf(stderr, "Error Creating SDl Window. \n");
+		return false;
+	}
+
+	// Creating SDl renderer
+	p_renderer = SDL_CreateRenderer(p_window, -1, 0);
+	if (!p_renderer) {
+		fprintf(stderr, "Error Creating SDl Renderer. \n");
+		return false;
+	}
+
+	SDL_SetWindowFullscreen(p_window, SDL_WINDOW_FULLSCREEN);
+	return true;
+
+
+}
+
+
+void render_color_buffer(void) {
+	SDL_UpdateTexture(
+		p_color_buffer_texture,
+		NULL,
+		Uint32_color_buffer,
+		(int)(i_Windown_width * sizeof(uint32_t))
+
+	);
+
+	SDL_RenderCopy(
+		p_renderer,
+		p_color_buffer_texture,
+		NULL,
+		NULL
+	);
+
+}
+// 
+// draw grid on Screen
+void draw_grid(void) {
+	for (int y = 0; y < i_Windown_height; y += 10) {
+		for (int x = 0; x < i_Windown_width; x += 10) {
+
+			Uint32_color_buffer[(i_Windown_width * y) + x] = 0xFF333333;
+
+		}
+	}
+
+}
+//
+
+
+void draw_react(int x, int y, int w, int h, uint32_t color) {
+	for (int i = x; i < x + w; i++) {
+		for (int j = y; j < y + h; j++) {
+			Uint32_color_buffer[(i_Windown_width * j) + i] = color;
+		}
+	}
+}
+
+//
+void clear_color_buffer(uint32_t color) {
+	for (int y = 0; y < i_Windown_height; y++) {
+		for (int x = 0; x < i_Windown_width; x++) {
+			Uint32_color_buffer[(i_Windown_width * y) + x] = color;
+		}
+	}
+}
+//
+
+
+//
+void destory_window(void) {
+	free(Uint32_color_buffer);
+	SDL_DestroyRenderer(p_renderer);
+	SDL_DestroyWindow(p_window);
+	SDL_Quit();
+}
+
+
