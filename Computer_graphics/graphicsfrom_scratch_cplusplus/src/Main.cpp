0d14e8d8aa653f1d36ec3eba170e8925b241d89f
Mohammed Yussif <mohammed.yussif@invennico.com>
2024-05-17 01:02:31 +0000

    Adding C++ project for Computer Graphics from sratch

---
+++ Computer_graphics/graphicsfrom_scratch_cplusplus/src/Main.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <SDL.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//The window we'll be rendering to
+SDL_Window* window = NULL;
+
+//The surface contained by the window
+SDL_Surface* screenSurface = NULL;
+
+int main(int argc, char* args[])
+{
+
+
+	
+
+	return 0;
+}
---
+++ Computer_graphics/graphicsfrom_scratch_cplusplus/src/headers/display.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Display {
+
+	class Display {
+	public:
+		Display();
+		~Display();
+	};
+
+}
