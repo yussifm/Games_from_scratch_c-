15a1addc01ad33fd524e08549850e46ad2168d76
yussifm <yussifm13@gmail.com>
2024-07-12 00:27:50 +0000

    Data Structures and Algo day 1

---
+++ Data_STruc_ALgo/structure_single_link_list.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <memory>
+
+using namespace std;
+
+struct Node {
+  int data;
+  Node *next;
+};
+
+int main() {
+
+  // Using Smart pointers
+  unique_ptr<Node> p;
+  p = make_unique<Node>();
+
+  p->data = 10;
+  p->next = NULL;
+
+  cout << p->data << endl;
+  cout << p->next << endl;
+
+  // Using Raw Pointers
+  Node *q;
+  q = new Node;
+  q->data = 20;
+  q->next = NULL;
+
+  cout << q->data << endl;
+  cout << q->next << endl;
+
+  delete q;
+  return 0;
+}
