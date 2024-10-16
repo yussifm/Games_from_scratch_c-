02e506f9b195acd4563d73e703c9485b0e2bdba3
yussifm <yussifm13@gmail.com>
2024-10-16 00:53:41 +0000

    type traits

---
+++ cc_intermie_advance/types_trsits_and_concepts/type_triats_examples.cpp
@@ -0,0 +1,41 @@
+// to use type traits we to include the header file
+#include <type_traits>
+#include <iostream>
+
+
+
+
+// Static in Structs and classes
+struct StructOneExam
+{
+  using type = bool;
+  static const int value{44}; 
+	
+};
+
+
+// Using Type Traits with Tenolate
+template <typename T>
+void FunctionOne(T Param) {
+  if (std::is_arithmetic_v<T>) {
+    std::cout << Param << " is arithmetic\n";
+  } else {
+    std::cout << Param << " is not arithmetic\n";
+  }
+}
+
+
+int main (){
+    
+    // this will be a boolean value
+    StructOneExam::type boolValue;
+      
+     // this is going to be an int value of 42
+    int num = StructOneExam::value;
+
+    std::cout<< num <<std::endl;
+
+
+    
+	return 0;
+}
