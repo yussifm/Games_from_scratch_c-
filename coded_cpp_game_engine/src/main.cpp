d4942357d788d607b223ddb04b890b72a72b06bb
yussifm <yussifm13@gmail.com>
2024-11-28 16:31:11 +0000

    .

--- coded_cpp_game_engine/src/main.cpp
+++ coded_cpp_game_engine/src/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
-
-int main() {
+#include <imgui/imgui.h>
+#include <imgui/imgui_sdl.h>
+#include <SDL2/SDL.h>
+int main(int argc, char* argv[]) {
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world!" << std::endl;
     std::cout << "Hello, world!" << std::endl;
     return 0;
 }
